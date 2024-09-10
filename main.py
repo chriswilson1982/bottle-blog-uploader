92fb696707015dfe9377b3fe7487704882d6cabc
Chris Wilson <c.m.wilson@me.com>
2024-09-10 23:39:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import blog_uploader # Custom routes
 
 app = Bottle()
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if os.getenv('APP_LOCATION') == 'heroku':
+	app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
 else:
     app.run(host='localhost', port=8080, debug=True)
